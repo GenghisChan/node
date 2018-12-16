982bcdf7c7e65f268d73f193e94183038623caa3
github username <github email address>
2018-12-16 15:48:39 -0500

    Passing in args

--- emmiters.js
+++ emmiters.js
@@ -1,8 +1,8 @@
 const EventEmitter = require('events');
 const eventEmitter = new EventEmitter();
 
-eventEmitter.on('broadcast', () => {
-  console.log('Broadcasting ...')
+eventEmitter.on('broadcast', (num1, num2) => {
+  console.log(num1 + num2)
 })
 
-eventEmitter.emit('broadcast')
+eventEmitter.emit('broadcast', 1, 2);
