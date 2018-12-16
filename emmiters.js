0a21a5c3b2b16bd08698d964d3506b6c9fa170ec
github username <github email address>
2018-12-16 16:21:12 -0500

    Inheritance with EventEmitter and emit executes synchronously

--- emmiters.js
+++ emmiters.js
@@ -6,3 +6,29 @@ eventEmitter.on('broadcast', (num1, num2) => {
 })
 
 eventEmitter.emit('broadcast', 1, 2);
+
+class Person extends EventEmitter{
+  constructor(name){
+    super();
+    this._name = name
+  }
+
+  get name(){
+    return this._name
+  }
+}
+
+let chris = new Person('chris');
+let karla = new Person('karla')
+
+chris.on('name', () => {
+  console.log("My name is", chris.name)
+})
+
+karla.on('name', () => {
+  console.log("My name is", karla.name)
+})
+
+
+chris.emit('name')
+karla.emit('name')
